3630cae8c1f94862af82d6f4150c986e0ba1852d
Jaibey Pisco <jaibey.pisco@tecsup.edu.pe>
2022-04-20 00:47:48 -0500

    proyecto listo

---
+++ start/db/database.sql
@@ -0,0 +1,13 @@
+create table employee(
+ id int(11) primary key AUTO_INCREMENT,
+name varchar(50),
+salary int(11)
+
+);
+
+Insert into  employee(
+    name, salary
+) VALUES("Jaibey", 2500),
+("Sterling", 1000),
+("Yelixsa", 2500),
+("Didian", 2500);
